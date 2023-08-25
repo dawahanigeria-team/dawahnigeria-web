c97bc6612815c307c9e75385addf5c02cae3a93f
Yusadolat <yusadolat@gmail.com>
2023-08-25 10:15:37 +0100

    Replace DN DSN

--- src/App.jsx
+++ src/App.jsx
@@ -39,19 +39,22 @@ export const AudioContext = createContext();
 export const SearchContext = createContext();
 
 Sentry.init({
-    dsn: "https://11ec8579dd28244e4ab11514ae11d06a@o4505351228424192.ingest.sentry.io/4505748101988352",
-    integrations: [
-        new Sentry.BrowserTracing({
-            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-            tracePropagationTargets: ["localhost", "https://www.dawahbox.com/mongo/api/"],
-        }),
-        new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: .5, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 0.1, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  dsn: 'https://39f51c39cd7f76985eac0998370570fb@o4505749236875264.ingest.sentry.io/4505764791451648',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        'localhost',
+        ''
+      ]
+    }),
+    new Sentry.Replay()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 0.1 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 const App = () => {
