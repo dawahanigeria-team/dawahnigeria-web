d1e9f0886cbc1c9b294f1cc1934f930f64e52faf
Yusadolat <yusadolat@gmail.com>
2023-09-07 21:59:52 +0100

    Added Sentry / Browser tracing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState, createContext, useEffect, useRef } from "react";
+import * as Sentry from '@sentry/react';
+
 import "./App.css";
 
 import { Toaster } from "react-hot-toast";
@@ -37,6 +39,21 @@ import ForgotPassword from "./pages/forgotpassword/forgotPassword";
 export const AudioContext = createContext();
 export const SearchContext = createContext();
 
+Sentry.init({
+  dsn: 'https://39f51c39cd7f76985eac0998370570fb@o4505749236875264.ingest.sentry.io/4505764791451648',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/]
+    }),
+    new Sentry.Replay()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.3, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 const App = () => {
   const scroll = useRef();
   const audioRef = useRef();
