002b345ea065365647b22f13b13fa6175817f9f2
jocanola <jokanola.it@gmail.com>
2023-09-13 21:58:06 +0100

    AG: tracking config

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // import Scrolltotop from "./components/UI/scrollToTop";
 
 //import InfiniteScroll from "./components/UI/infiniteScroll";
@@ -36,11 +36,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </Router>
   </>
 );
 
