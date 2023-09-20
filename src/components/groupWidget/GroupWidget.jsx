b8c4d534d929304f5427f3c35724dc018c1ad980
Yusadolat <yusadolat@gmail.com>
2023-09-20 08:24:14 +0100

    fixed

--- src/components/groupWidget/GroupWidget.jsx
+++ src/components/groupWidget/GroupWidget.jsx
@@ -57,18 +57,9 @@ const GroupWidget = ({
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [size]);
 
-  ////console.log(data);
-
   function prev() {
-    // e.stopPropagation()
-
-    ////console.log('window.scrollWidth')
-    //console.log(slide.current.scrollLeft);
-    //console.log(slide.current.scrollWidth);
-    //console.log(slide.current.offsetWidth);
     slide.current.scrollBy({
       left: -slide.current.scrollWidth / 10,
       behavior: "smooth",
@@ -76,11 +67,6 @@ const GroupWidget = ({
   }
 
   function next() {
-    //e.stopPropagation()
-    ////console.log('window.scrollWidth')
-
-    //console.log(slide.current.scrollWidth);
-    //console.log(slide.current.offsetWidth);
     slide.current.scrollBy({
       left: slide.current.scrollWidth / 10,
       behavior: "smooth",
@@ -89,9 +75,7 @@ const GroupWidget = ({
 
   useEffect(() => {
     function scrollEl() {
-    
-      //console.log("Slide");
-      if (slide.current.scrollLeft === 0) {
+          if (slide.current.scrollLeft === 0) {
         setisprev(false);
       } else {
         setisprev(true);
@@ -110,7 +94,6 @@ const GroupWidget = ({
     slide.current?.addEventListener("scroll", scrollEl);
 
     return () => slide.current?.removeEventListener("scroll", scrollEl);
-     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [slide.current?.scrollLeft]);
 
   /**
@@ -254,72 +237,73 @@ const GroupWidget = ({
                 styling ? "min-[615px]:space-x-3 space-x-3" : ""
               }`}
             >
-              {data.map(
-                (
-                  {
-                    img,
-                    lec_img,
-                    categories,
-                    cats,
-                    id,
-                    mp3_title,
-                    title,
-                    Title,
-                    rpname,
-                    nid,
-                    audio,
-                    views,
-                  },
-                  idx
-                ) => {
-                  return (
-                    <div key={idx}>
-                      <Link
-                        to={`${LECTURE}${nid || id}`}
-                        id={idx}
-                        className={`groupWidget_album_item  ${
-                          styling ? "relative max-[615px]:hidden" : ""
-                        }`}
-                        onClick={() => {
-                          // navigate(`/l/${nid || id}`);
-                          if (window.innerWidth <= 615) {
-                            dispatch(getPack(null));
-                            dispatch(getPage(currentPage));
-                            dispatch(getCount(idx));
-                            dispatch(getPack(data));
-                            setinitial(false);
-                          }
-                        }}
-                        key={idx + 1}
-                      >
-                        <LandingWidget
-                          key={idx}
-                          categories={
-                            title?.split("-")[0] ||
-                            Title?.split("-")[0] ||
-                            title ||
-                            mp3_title
-                          }
-                          img={img || lec_img}
-                          views={views || 0}
-                          nid={nid || id}
-                          styling={styling}
-                        />
-                        <div
-                          className={`absolute right-[-14px] bottom-[10px] rounded-full h-[38px] w-[38px] flex justify-center items-center text-white text-xl ${
-                            idx === 2 ? "bg-[#96734a]" : ""
-                          } ${idx === 1 ? "bg-[#76a8d7]" : ""}${
-                            idx === 0 ? "bg-[#ffa736]" : ""
-                          }
-                        ${styling && idx < 3 ? "block" : "hidden"}`}
+              {Array.isArray(data) &&
+                data.map(
+                  (
+                    {
+                      img,
+                      lec_img,
+                      categories,
+                      cats,
+                      id,
+                      mp3_title,
+                      title,
+                      Title,
+                      rpname,
+                      nid,
+                      audio,
+                      views,
+                    },
+                    idx
+                  ) => {
+                    return (
+                      <div key={idx}>
+                        <Link
+                          to={`${LECTURE}${nid || id}`}
+                          id={idx}
+                          className={`groupWidget_album_item  ${
+                            styling ? "relative max-[615px]:hidden" : ""
+                          }`}
+                          onClick={() => {
+                            // navigate(`/l/${nid || id}`);
+                            if (window.innerWidth <= 615) {
+                              dispatch(getPack(null));
+                              dispatch(getPage(currentPage));
+                              dispatch(getCount(idx));
+                              dispatch(getPack(data));
+                              setinitial(false);
+                            }
+                          }}
+                          key={idx + 1}
                         >
-                          <span>{idx + 1}</span>
-                        </div>
-                      </Link>
-                    </div>
-                  );
-                }
-              )}
+                          <LandingWidget
+                            key={idx}
+                            categories={
+                              title?.split("-")[0] ||
+                              Title?.split("-")[0] ||
+                              title ||
+                              mp3_title
+                            }
+                            img={img || lec_img}
+                            views={views || 0}
+                            nid={nid || id}
+                            styling={styling}
+                          />
+                          <div
+                            className={`absolute right-[-14px] bottom-[10px] rounded-full h-[38px] w-[38px] flex justify-center items-center text-white text-xl ${
+                              idx === 2 ? "bg-[#96734a]" : ""
+                            } ${idx === 1 ? "bg-[#76a8d7]" : ""}${
+                              idx === 0 ? "bg-[#ffa736]" : ""
+                            }
+                        ${styling && idx < 3 ? "block" : "hidden"}`}
+                          >
+                            <span>{idx + 1}</span>
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  }
+                )}
             </div>
           </div>
         </div>
@@ -445,51 +429,50 @@ const GroupWidget = ({
           </div>
           <div ref={slide} className="overflow_auto_wrapper">
             <div className="overflow_auto_after">
-              {data.map(
-                (
-                  {
-                    img,
-                    lec_img,
-                    categories,
-                    cats,
-                    title,
-                    nid,
-                    Title,
-                    rpname,
-                    name,
-                    playlist_img,
-                    id,
-                    audio,
-                    views,
-                  },
-                  idx
-                ) => {
-                  return (
-                    <Link
-                      to={`${PLAYLISTS}${id || nid}`}
-                      id={idx}
-                      className="groupWidget_album_item"
-                      onClick={() => {
-                        // navigate(`/pl/${id || nid}`);
-                      }}
-                      key={idx + 1}
-                    >
-                      <LandingWidget
-                        key={idx}
-                        categories={
-                          name ||
-                          title?.split("-")[0] ||
-                          Title?.split("-")[0] ||
-                          title
-                        }
-                        img={lec_img}
-                        views={views || 0}
-                        nid={id || nid}
-                      />
-                    </Link>
-                  );
-                }
-              )}
+              {Array.isArray(data) &&
+                data.map(
+                  (
+                    {
+                      img,
+                      lec_img,
+                      categories,
+                      cats,
+                      title,
+                      nid,
+                      Title,
+                      rpname,
+                      name,
+                      playlist_img,
+                      id,
+                      audio,
+                      views,
+                    },
+                    idx
+                  ) => {
+                    return (
+                      <Link
+                        to={`${PLAYLISTS}${id || nid}`}
+                        id={idx}
+                        className="groupWidget_album_item"
+                        onClick={() => {}}
+                        key={idx + 1}
+                      >
+                        <LandingWidget
+                          key={idx}
+                          categories={
+                            name ||
+                            title?.split("-")[0] ||
+                            Title?.split("-")[0] ||
+                            title
+                          }
+                          img={lec_img}
+                          views={views || 0}
+                          nid={id || nid}
+                        />
+                      </Link>
+                    );
+                  }
+                )}
             </div>
           </div>
         </div>
