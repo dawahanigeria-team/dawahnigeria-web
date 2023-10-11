e9f02888cdfc7548fa3fd800dffe87df867d47f9
jocanola <jokanola.it@gmail.com>
2023-10-11 11:31:30 +0100

    UX: Add skeleton loading on landing page

---
+++ src/components/skeletion/skeleton.container.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CardSkeleton from ".";
+
+const RowSkeletonContainer = () => {
+  return (
+    <div className="groupWidget_wrapper">
+      <div className="groupWidget_top">
+        <p className="groupWidget_top_heading">Loading...</p>
+      </div>
+      <div className="overflow_hidden_wrapper">
+        <div className="overflow_auto_wrapper">
+          <div
+            className={`overflow_auto_after
+            `}
+          >
+            {Array(10)
+              .fill(undefined)
+              .map((_, i) => {
+                return <CardSkeleton key={i} />;
+              })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(RowSkeletonContainer);
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./landing.scss";
 import Container from "../../components/container/Container";
 import GroupWidget from "../../components/groupWidget/GroupWidget";
 import axios from "../../utils/useAxios";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import bchart from "../../assets/svg/boom-chart.svg";
 import blecturer from "../../assets/svg/boom-lecturer.svg";
-
 import bplaylist from "../../assets/svg/boom-playlist.svg";
 import btrending from "../../assets/svg/boom-trending.svg";
 import bnew from "../../assets/svg/boom-new.svg";
@@ -19,6 +18,8 @@ import useAxios from "axios";
 import LandingOptions from "../../components/landingOptions/LandingOptions";
 import MyCarousel from "../../components/UI/carousel/myCarousel";
 import MobileImageWidget from "./mobileimagewidget/mobileImageWidget";
+import { settings, settings1 } from "./utils";
+import CarouselSkeleton from "../../components/skeletion/carousel.skeleton";
 import {
   GENRES,
   HOME,
@@ -31,13 +32,12 @@ import {
   LECTURERS,
 } from "../../utils/routes/constants";
 import HeadMeta from "../../components/head-meta";
+import RowSkeletonContainer from "../../components/skeletion/skeleton.container";
 const Landing = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [recent, setRecent] = useState([]);
   const [curPlay, setcurPlay] = useState([]);
-  const [trending, setTrending] = useState([]);
-  const [ramadan, setRamadan] = useState([]);
-  const [quran, setQuran] = useState([]);
+  const [isSpecialFeatLoading, setIsSpecialFeatLoading] = useState(false);
   const [isrecent, setisrecent] = useState(false);
   const [images, setimages] = useState([]);
   const [specailFeat, setSpecialFeat] = useState([]);
@@ -55,15 +55,44 @@ const Landing = () => {
       });
   }, []);
 
+  const fetSpecialFeat = () => {
+    setIsSpecialFeatLoading(true);
+    useAxios
+      .post(
+        `${process.env.REACT_APP_API_ADMINISTER_BASE_URL}/spcl_ftr_api.php`,
+        {
+          action: "retrieve_spcl_ftr_data",
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "x-project": "206cf92c-8a46-45ef-bf3f-a6ef92fc6f25",
+          },
+        }
+      )
+      .then((res) => {
+        setIsSpecialFeatLoading(false);
+        const specialFeatures = res.data?.flatMap((val) => [
+          { name: val.name, more: val.more },
+        ]);
+        setSpecialFeat(specialFeatures);
+      })
+      .catch((err) => {
+        setIsSpecialFeatLoading(false);
+
+        console.log(err);
+      });
+  };
+
   let page = 1;
   useEffect(() => {
-    async function fetchData() {
+    function fetchData() {
+      setisrecent(true);
       if (currentUser?.id) {
-        await axios
+        axios
           .get(`/recentApi.php?user_id=${currentUser?.id}&action=get_recent`)
           .then((res) => {
-            //console.log('user recent',res.data);
-
             if (res.data?.length === 0) {
               //recent
               setisrecent(false);
@@ -99,130 +128,87 @@ const Landing = () => {
           });
       } else {
         //recent
-        setisrecent(false);
-        await axios
+        axios
           .get(`/leclisting_lang.php?langid=6&page=${page}`)
           .then((res) => {
+            setisrecent(false);
             setRecent(res.data?.slice(0, 10));
             // //console.log("trending: ", res.data);
           })
           .catch((err) => {
+            setisrecent(false);
             //console.log(err);
           });
       }
-
-      await useAxios
-        .post(
-          `${process.env.REACT_APP_API_ADMINISTER_BASE_URL}/spcl_ftr_api.php`,
-          {
-            action: "retrieve_spcl_ftr_data",
-          },
-          {
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              "x-project": "206cf92c-8a46-45ef-bf3f-a6ef92fc6f25",
-            },
-          }
-        )
-        .then((res) => {
-          console.log(res.data);
-          const specialFeatures = res.data?.flatMap((val) => [
-            { name: val.name, more: val.more },
-          ]);
-          setSpecialFeat(specialFeatures);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
     }
+
+    fetSpecialFeat();
     fetchData();
   }, []);
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    fade: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: false,
-    nextArrow: false,
-  };
-  const settings1 = {
-    dots: false,
-    infinite: false,
-    autoplay: false,
-    fade: false,
-    speed: 500,
-    slidesToShow: 6,
-    swipeToSlide: true,
-    slidesToScroll: 1,
-    prevArrow: false,
-    nextArrow: false,
-    responsive: [
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   return (
     <Container>
       <HeadMeta title="DawahNigeria | Home" />
       <div className="landing_wrapper px-[2%] max-[615px]:py-[5%] py-[8%] min-[690px]:py-[2%]">
-        <div className="carousel  h-[250px] min-[950px]:h-[250px] min-[1050px]:h-[250px] min-[1283px]:h-[300px]">
-          <MyCarousel images={images} />
-        </div>
+        {images.length > 1 ? (
+          <>
+            <div className="carousel  h-[250px] min-[950px]:h-[250px] min-[1050px]:h-[250px] min-[1283px]:h-[300px]">
+              <MyCarousel images={images} />
+            </div>
 
-        <Slider className="landing_carousel landing_space" {...settings}>
-          {images?.map((image, index) => {
-            return (
-              <div key={index} className="landing_carousel_img">
-                <MobileImageWidget image={image} className="" />
-              </div>
-            );
-          })}
-        </Slider>
-        <Slider className="landing_options" {...settings1}>
-          <LandingOptions text={"Charts"} img={bchart} link={CHARTS} />
-          <LandingOptions text={"Lecturers"} img={blecturer} link={LECTURERS} />
-          <LandingOptions text={"Playlists"} img={bplaylist} link={PLAY} />
-          <LandingOptions
-            text={"Video"}
-            icon={<BsFillPlayBtnFill />}
-            link={VIDEO}
-          />
-          <LandingOptions text={"Genre"} img={bgenre} link={GENRES} />
-          <LandingOptions text={"Trending"} img={btrending} link={TRENDING} />
-          <LandingOptions text={"New"} img={bnew} link={NEW} />
-        </Slider>
+            <Slider className="landing_carousel landing_space" {...settings}>
+              {images?.map((image, index) => {
+                return (
+                  <div key={index} className="landing_carousel_img">
+                    <MobileImageWidget image={image} className="" />
+                  </div>
+                );
+              })}
+            </Slider>
+            <Slider className="landing_options" {...settings1}>
+              <LandingOptions text={"Charts"} img={bchart} link={CHARTS} />
+              <LandingOptions
+                text={"Lecturers"}
+                img={blecturer}
+                link={LECTURERS}
+              />
+              <LandingOptions text={"Playlists"} img={bplaylist} link={PLAY} />
+              <LandingOptions
+                text={"Video"}
+                icon={<BsFillPlayBtnFill />}
+                link={VIDEO}
+              />
+              <LandingOptions text={"Genre"} img={bgenre} link={GENRES} />
+              <LandingOptions
+                text={"Trending"}
+                img={btrending}
+                link={TRENDING}
+              />
+              <LandingOptions text={"New"} img={bnew} link={NEW} />
+            </Slider>
+          </>
+        ) : (
+          <CarouselSkeleton />
+        )}
 
-        <div className="landing_recent landing_space my-1 min-[615px]:my-3">
-          {" "}
-          <GroupWidget
-            data={recent}
-            heading="Recent"
-            type={"recent"}
-            endpoint_url={"/leclisting_lang.php?langid=6&page="}
-            currentPage={page}
-            previousPlay={curPlay}
-            isrecent={isrecent}
-            nav1={{ title: "Home", link: HOME }}
-          />
-        </div>
+        {recent && !isrecent ? (
+          <div className="landing_recent landing_space my-1 min-[615px]:my-3">
+            <GroupWidget
+              data={recent}
+              heading="Recent"
+              type={"recent"}
+              endpoint_url={"/leclisting_lang.php?langid=6&page="}
+              currentPage={page}
+              previousPlay={curPlay}
+              isrecent={isrecent}
+              nav1={{ title: "Home", link: HOME }}
+            />
+          </div>
+        ) : (
+          <div className="landing_recent landing_space my-1 min-[615px]:my-3">
+            <RowSkeletonContainer />
+          </div>
+        )}
 
         {Array.isArray(specailFeat) &&
           specailFeat?.map(({ name, more }, idx) => {
@@ -243,6 +229,17 @@ const Landing = () => {
               );
             }
           })}
+        {isSpecialFeatLoading &&
+          Array(10)
+            .fill(undefined)
+            .map((_, i) => (
+              <div
+                key={i}
+                className="landing_recent landing_space my-1 min-[615px]:my-3"
+              >
+                <RowSkeletonContainer />
+              </div>
+            ))}
       </div>
     </Container>
   );
