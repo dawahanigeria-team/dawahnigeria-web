8aab22d487b6bd3d90ee5da63b8dec6d6b79702e
cradoe <noblewapinfo@gmail.com>
2023-10-14 07:04:14 +0100

    chore: added default og image

--- src/components/head-meta/index.js
+++ src/components/head-meta/index.js
@@ -1,20 +1,29 @@
 import { Helmet } from "react-helmet";
 import React from "react";
 
-const HeadMeta = ({ title, description }) => {
+const HeadMeta = ({
+  title,
+  description,
+  ogImage = "https://pub-09f814adc0704e7db8ea3d3ad843eb7e.r2.dev/dn-banner.jpeg",
+}) => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
       <meta
         name="description"
         content={
-          description ??
+          description ||
           "Explore Islamic educational resources like articles, lectures, videos, and e-books on Dawah Nigeria. Promoting knowledge and guidance for all."
         }
       />
-      <meta name="keywords" content="" />
+      <meta
+        name="keywords"
+        content="Islamic education, Dawah Nigeria, articles, lectures, videos, e-books, Islam, guidance, knowledge, online platform, religious resources, Quran, Islamic teachings"
+      />
+      <meta property="og:image" content={ogImage} />
+
       <meta name="author" content="Edawah" />
-      <title>{title ?? "Dawah Nigeria"}</title>
+      <title>{title || "Dawah Nigeria"}</title>
     </Helmet>
   );
 };
