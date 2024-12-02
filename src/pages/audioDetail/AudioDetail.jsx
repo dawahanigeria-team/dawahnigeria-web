a7d7d4d5443dbefc4065461d11d0fd0e51edd86c
DevSaalih <soliualley@gmail.com>
2024-12-02 23:02:33 +0100

    More Option fix

--- src/pages/audioDetail/AudioDetail.jsx
+++ src/pages/audioDetail/AudioDetail.jsx
@@ -644,13 +644,7 @@ const AudioDetail = () => {
                     More
                   </div>
                 </div>
-                <div
-                  className={
-                    moreOption
-                      ? "right-0 top-10  absolute min-w-max h-fit"
-                      : "hidden"
-                  }
-                >
+                {moreOption && (
                   <span
                     onClick={(e) => {
                       e.stopPropagation();
@@ -658,11 +652,19 @@ const AudioDetail = () => {
                     }}
                     className="fixed inset-0 bg-none z-[90] w-full h-full"
                   ></span>
+                )}
+                <div
+                  className={
+                    moreOption
+                      ? "right-0 top-10 z-[200] absolute min-w-max h-fit"
+                      : "hidden"
+                  }
+                >
                   <div
                     onClick={(e) => {
                       e.stopPropagation();
                     }}
-                    className="bg-background shadow-lg border z-[200]  rounded-sm space-y-2 p-1 font-light text-[12px] text-foreground"
+                    className="bg-background shadow-lg border   rounded-sm space-y-2 p-1 font-light text-[12px] text-foreground"
                   >
                     <span
                       onClick={(e) => {
