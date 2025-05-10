9384b85ed5b13e835a341e02e4482a25577c7142
Yusadolat <Yusadolat@gmail.com>
2025-05-10 21:09:15 +0100

    refactor: Simplify API service call in getNewLectures function

--- src/services/new.service.js
+++ src/services/new.service.js
@@ -2,7 +2,7 @@ import { apiService } from "./api";
 
 export const newApi = {
   getNewLectures: async ({ page }) =>
-    await apiService(process.env.REACT_APP_DEV_API_BASE_URL).get(
+    await apiService().get(
       `/leclisting_recent.php?&action=get_recent_audio&page=${page}`
     ),
 };
